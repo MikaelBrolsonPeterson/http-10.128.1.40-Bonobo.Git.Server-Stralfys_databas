c6692d5a764ba7537d87567727ebaff0356af4df
Mikael Peterson <156621@skane.se>
2020-10-09 09:34:27 +0200

    First commit

---
+++ Stralfys_databas_proj/Slutna_stralkallor.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[slutna_stralkallor] (
+    [Id]                   INT            IDENTITY (1, 1) NOT NULL,
+    [Isotop_ID]            INT            NULL,
+    [Kalibreringsdatum]    DATETIME       NULL,
+    [ReferensAktivitetMBq] FLOAT (53)     NULL,
+    [Serienummer]          NVARCHAR (50)  NULL,
+    [Tillverkare]          NVARCHAR (50)  NULL,
+    [Leverantor]           NVARCHAR (50)  NULL,
+    [Formfaktor]           NVARCHAR (50)  NULL,
+    [Dimensioner]          NVARCHAR (50)  NULL,
+    [Rum]                  NVARCHAR (50)  NULL,
+    [Avdelning]            NVARCHAR (50)  NULL,
+    [Sjukhus]              NVARCHAR (50)  NULL,
+    [StrykprovVidLeverans] BIT            NULL,
+    [KontrollIntervall]    INT            NULL,
+    [Kommentar]            NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
